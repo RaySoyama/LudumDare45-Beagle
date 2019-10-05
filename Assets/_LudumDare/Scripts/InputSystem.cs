13de8c42bdeaa617b0d4959703e816303ef18c69
Ray Soyama <raysoyama@gmail.com>
2019-10-05 00:16:01 -0700

    Input System creation, start

---
+++ Assets/_LudumDare/Scripts/InputSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSystem : MonoBehaviour
+{
+    [SerializeField] [ReadOnlyField]
+    private Vector2 directionalInput;
+    public Vector2 DirectionalInput
+    {
+        get 
+        {
+            return directionalInput;
+        }
+    }
+
+    [SerializeField] [ReadOnlyField]
+    private float forwardValue;
+    public float ForwadValue
+    {
+        get 
+        {
+            return forwardValue;
+        }
+    }
+
+    [SerializeField][ReadOnlyField]
+    private float sidewayValue;
+    public float SidewayValue
+    {
+        get
+        {
+           return sidewayValue;
+        }
+    }
+
+    [SerializeField]
+    private KeyCode forwardKey = KeyCode.W;
+    [SerializeField]
+    private KeyCode backwardKey = KeyCode.S;
+    [SerializeField]
+    private KeyCode rightwardKey = KeyCode.D;
+    [SerializeField]
+    private KeyCode leftwardKey = KeyCode.A;
+
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_LudumDare/Scripts/WoofMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoofMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed;
+
+    
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
