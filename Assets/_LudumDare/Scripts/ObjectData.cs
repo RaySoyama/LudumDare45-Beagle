b1766aad76c2ca2b7b314b83df5f22999d63386c
Ray Soyama <raysoyama@gmail.com>
2019-10-05 18:45:44 -0700

    enum for objects

---
+++ Assets/_LudumDare/Scripts/ObjectData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectData : MonoBehaviour
+{
+    public enum ObjectType
+    {
+        SmallStick,
+        BigStick,
+    }
+
+    [SerializeField]
+    private ObjectType type;
+    public ObjectType Type
+    {
+        get
+        {
+            return type;
+        }
+    }
+
+    public List<Transform> grabPoints;
+}
