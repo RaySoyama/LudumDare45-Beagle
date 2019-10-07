e4fed464c7902971e007f0f5bff01e746fbaf971
Ray Soyama <raysoyama@gmail.com>
2019-10-07 03:12:47 -0700

    sit and idle systm working

--- Assets/_LudumDare/Scripts/TutorialSystem.cs
+++ Assets/_LudumDare/Scripts/TutorialSystem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialSystem : MonoBehaviour
 {
@@ -18,22 +19,188 @@ public class TutorialSystem : MonoBehaviour
      * 11.Give meat
     s */
 
+    [System.Serializable]
+    private struct Icons
+    {
+       public Sprite ControllerIcon;
+       public Sprite KeyboardIcon;
+    }
+
+    private enum Tutorial
+    { 
+        Idle,
+        Sit,
+        Bark,
+        SpawnStick,
+        PickUp,
+        ThrowStick,
+        Run,
+        ReturnStick,
+        GiveMeat,
+        End
+    }
+
     [SerializeField]
-    private Animator anim;
+    private float clapDuration;
+
+    [SerializeField]
+    private Animator manAnim;
+    
+    [SerializeField]
+    private Animator dogAnim;
+
+    [SerializeField]
+    private Image TutorialIcons;
 
 
     [SerializeField]
+    private Icons movementIcons;
+    
+    [SerializeField]
+    private Icons sitIcons;
 
 
 
+
+    [SerializeField] [ReadOnlyField]
+    private List<Tutorial> TutorialAction = new List<Tutorial>();
+
+    [SerializeField]
+    private bool dogInRange;
+
+
+    private Coroutine ClapCour;
 
     void Start()
     {
-        
+        TutorialAction = new List<Tutorial>() { Tutorial.Idle,Tutorial.Sit,Tutorial.Bark,
+                                                Tutorial.SpawnStick,Tutorial.PickUp,Tutorial.ThrowStick,
+                                                Tutorial.Run,Tutorial.ReturnStick,Tutorial.GiveMeat,Tutorial.End};
+
+
     }
 
     void Update()
     {
-        
+        switch (TutorialAction[0])
+        {
+            case Tutorial.Idle:
+                IdleUpdate();
+                break;
+            case Tutorial.Sit:
+                SitUpdate();
+                break;
+            case Tutorial.Bark:
+                break;
+            case Tutorial.SpawnStick:
+                break;
+            case Tutorial.PickUp:
+                break;
+            case Tutorial.ThrowStick:
+                break;
+            case Tutorial.Run:
+                break;
+            case Tutorial.ReturnStick:
+                break;
+            case Tutorial.GiveMeat:
+                break;
+            case Tutorial.End:
+                break;
+
+        }
     }
+
+
+    private void IdleUpdate()
+    {
+
+        if (InputSystem.WoofInput.inputMode == InputSystem.InputMode.Controller)
+        {
+            TutorialIcons.sprite = movementIcons.ControllerIcon;
+        }
+        else
+        {
+            TutorialIcons.sprite = movementIcons.KeyboardIcon;
+        }
+
+        //animation shit
+        //humm or make noise
+
+        //check for dog
+        if (dogInRange == true)
+        {
+            TutorialAction.RemoveAt(0);
+        }
+    }
+
+    private void SitUpdate()
+    {
+        //Spawn Icon
+        if (InputSystem.WoofInput.inputMode == InputSystem.InputMode.Controller)
+        {
+            TutorialIcons.sprite = sitIcons.ControllerIcon;
+        }
+        else
+        {
+            TutorialIcons.sprite = sitIcons.KeyboardIcon;
+        }
+
+        //detect sitting
+        if (dogAnim.GetBool("isSitting") == true)
+        {
+            if (ClapCour == null)
+            {
+                ClapCour = StartCoroutine(ClapCoroutine());
+            }
+        }
+    }
+
+
+
+    private IEnumerator ClapCoroutine()
+    {
+        //set clap anim on
+        manAnim.SetBool("isClapping", true);
+
+
+        yield return new WaitForSeconds(clapDuration);
+        manAnim.SetBool("isClapping", false);
+
+        //set clap anim off
+
+        
+
+        ClapCour = null;
+        TutorialAction.RemoveAt(0);
+    }
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") == true)
+        {
+            dogInRange = true;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") == true)
+        {
+            dogInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") == true)
+        {
+            dogInRange = false;
+        }
+
+
+    }
+
+
 }
